6b239543eb2773a7c6bfced32592e6ef8ef1af5f
vickyleonardo23 <vickyleonardo23@gmail.com>
2024-07-16 09:31:14 +0000

    call all database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,9 @@ class DatabaseSeeder extends Seeder
         // ]);
        $this->call([
         UserSeeder::class, 
-        UserVerificationSeeder::class]);
+        UserVerificationSeeder::class,
+        CourseSeeder::class,
+    ]);
+        
     }
 }
