7979e28cda923b8999dc6818c8e4a0a2d7cf7546
vickyleonardo23 <vickyleonardo23@gmail.com>
2024-07-16 09:31:25 +0000

    create partials

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -13,5 +13,24 @@
                 <span>Course</span>
             </a>
         </li>
+        <li class="sidebar-item {{Route::is('admin.instructor*') ? 'active':''}}">
+            <a href="{{route('admin.instructor')}}" class='sidebar-link '>
+                <i class="bi bi-file-earmark-medical-fill"></i>
+                <span>Instructor</span>
+            </a>
+        </li>
+        <li class="sidebar-item {{Route::is('admin.instr.course*') ? 'active':''}}">
+            <a href="{{route('admin.instructor')}}" class='sidebar-link '>
+                <i class="bi bi-file-earmark-medical-fill"></i>
+                <span>Instructor Course</span>
+            </a>
+        </li>
+        <li class="sidebar-title">Application</li>
+        <li class="sidebar-item">
+            <a href="" class='sidebar-link '>
+                <i class="bi bi-file-earmark-medical-fill"></i>
+                <span>Pre Application</span>
+            </a>
+        </li>
     </ul>
 </div>
