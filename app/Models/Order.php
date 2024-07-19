f749ac8faaa9cc65a9756ff1d29f0d4501f7ad4d
vickyleonardo23 <vickyleonardo23@gmail.com>
2024-07-19 09:36:07 +0000

    add order controller

---
+++ app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function year(){
+        return $this->belongsTo(Year::class);
+    }
+
+    public function orderItems(){
+        return $this->hasMany(OrderItem::class);
+    }
+    
+    public function course(){
+        return $this->belongsToMany(Course::class, 'order_items')->withPivot('harga');
+    }
+}
